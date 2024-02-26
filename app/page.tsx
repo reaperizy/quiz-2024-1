2b3a2250d9a7ecb9e285b6f96fcd260511ecd66a
Wadi Wahyudin <wadi.wahyudin7@gmail.com>
2024-02-26 13:53:03 +0700

    adding Image, and use Router Push (POST) for handle button + create button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+//Components
+import Button from "@/components/Button/Button";
+
+//Homepage Image
+import HomePageImage from "@/assets/home-pic.png"
+
 const Home = () => {
+  const router = useRouter();
+  //const startQuiz = () => router.push("/quiz");
+
+  const handleButtonClick = () => {
+    router.push("/quiz");
+  };
+
   return (
     <div className='text-center'>
-      START HERE!
+      <p className="text-white p-4">Do you have what it takes to became the React-Quiz master?</p>
+      <Image className="max-w-[700px] w-full rounded-[10px]" src={HomePageImage} alt="home-page"></Image>
+      <p className="text-[#9f50ac] pt-2 pb-8 text-[10px]">Image from Unsplash by Wadi</p>
+      <Button text="Start Quiz" onClick={handleButtonClick} />
     </div>
   );
 };
---
+++ components/Button/Button.tsx
@@ -0,0 +1,24 @@
+type Props = {
+    text: string
+    onClick: () => void
+}
+
+const Button = ({ text, onClick }: Props) => {
+    return (
+        <button
+            className="
+                bg-[#9f50ac] 
+                select-none 
+                font-bold 
+                h-[45px] 
+                min-w-[120px] 
+                rounded-[10px] 
+                text-white"
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button
